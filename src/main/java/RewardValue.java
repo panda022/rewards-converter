4670dcb2fa8640df8668ec3aebb772c492aebf05
Pan Gong <pn819328@dal.ca>
2023-06-16 12:56:50 -0300

    Update RewardValueTest class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,16 +1,16 @@
 public class RewardValue {
     private double cash;
     private int miles;
-    private static double exchangeRate;
+    private static double exchangeRate = 0.0035;
 
     public RewardValue(double cash) {
         this.cash = cash;
-        this.miles = (int) (cash * exchangeRate);
+        this.miles = (int) (cash / exchangeRate);
     }
 
     public RewardValue(int miles) {
         this.miles = miles;
-        this.cash = miles / exchangeRate;
+        this.cash = miles * exchangeRate;
     }
 
     public double getMilesValue(){
